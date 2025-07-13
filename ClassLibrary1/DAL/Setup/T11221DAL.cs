321589fe7b4c3e60ca819bca4a0ba381895ec680
Mohammad Sadikur Rahman <32734077+rsadikur66@users.noreply.github.com>
2025-07-13 18:07:22 +0600

    category setup page start

---
+++ ClassLibrary1/DAL/Setup/T11221DAL.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.DAL.Setup
+{
+    public class T11221DAL : CommonDAL
+    {
+        public DataTable GetCategoryList()
+        {
+            return Query($"SELECT * FROM T11221");
+        }
+    }
+}
---
+++ Controllers/Setup/T11221Controller.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1.DAL.Setup;
+using Newtonsoft.Json;
+using System.Web.Mvc;
+
+namespace EcommerceWithChatGpt.Controllers.Setup
+{
+    public class T11221Controller : Controller
+    {
+        T11221DAL t11221Dal = new T11221DAL();
+        // GET: T11221
+        [HttpPost]
+        public ActionResult GetCatList()
+        {
+            var data = t11221Dal.GetCategoryList();
+            string jsonData = JsonConvert.SerializeObject(data);
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
